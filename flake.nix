acfa28701e06e3fbbac4ddb0d1c2b2d8681fc690
Ludw <61082023+cooukiez@users.noreply.github.com>
2026-05-10 03:37:55 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,33 +2,24 @@
   description = "PWA Desktop Nix";
   inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
 
-  outputs =
-    { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, ... }:
     let
-      supportedSystems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
     in
     {
-      packages = forAllSystems (
-        system:
+      packages = forAllSystems (system:
         let
-          configData = builtins.readFile ./config.json;
-          configJson=builtins.fromJSON (configData);
+          pkgs = import nixpkgs { inherit system; };
+          configJson = builtins.fromJSON (builtins.readFile ./config.json);
           name = configJson.name;
-          pkgs = import nixpkgs {
-            inherit system;
-          };
+          
           desktopFile = ''
             [Desktop Entry]
             Type=Application
             Name=${name}
             Exec=$out/bin/${name}
-            Icon=$out/share/pixmaps/${name}.svg
+            Icon=${name}
             Terminal=false
             Keywords=${configJson.keywords}
             Comment=${configJson.description}
@@ -42,19 +33,24 @@
 
             npmDepsHash = "sha256-3tIHllTGByjFaRW3piJJtkuuzRhAnJ+P76OEJ9HbCDM=";
             dontNpmBuild = true;
+
+            # CRITICAL: This makes 'makeWrapper' available in postInstall
+            nativeBuildInputs = [ pkgs.makeWrapper ];
+
             env = {
               ELECTRON_SKIP_BINARY_DOWNLOAD = 1;
             };
 
             postInstall = ''
+              # Create the binary wrapper
               makeWrapper ${pkgs.electron}/bin/electron $out/bin/${pname} \
                 --add-flags $out/lib/node_modules/${pname}/src/main.js \
                 --set PWA_URL "${configJson.url}"
 
               mkdir -p $out/share/pixmaps
-              cp $out/lib/node_modules/${pname}/assets/icon.svg $out/share/pixmaps/${pname}.svg
-
               mkdir -p $out/share/applications
+
+              cp $out/lib/node_modules/${pname}/assets/icon.svg $out/share/pixmaps/${pname}.svg
               echo "${desktopFile}" > $out/share/applications/${pname}.desktop
             '';
 
