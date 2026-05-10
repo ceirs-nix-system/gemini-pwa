7735fa1609a79381a9c3ee3ac7d595a2a023450a
Ludw <61082023+cooukiez@users.noreply.github.com>
2026-05-10 12:36:20 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -4,7 +4,13 @@
 
   outputs = { self, nixpkgs, ... }:
     let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
     in
     {
