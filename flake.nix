96e6ce0c6cbdcbdefb25140f9a7c32b2a183aed6
Ludw <61082023+cooukiez@users.noreply.github.com>
2026-05-10 12:20:39 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -33,6 +33,9 @@
 
             npmDepsHash = "sha256-3tIHllTGByjFaRW3piJJtkuuzRhAnJ+P76OEJ9HbCDM=";
             dontNpmBuild = true;
+
+            nativeBuildInputs = [ pkgs.makeWrapper ];
+
             env = {
               ELECTRON_SKIP_BINARY_DOWNLOAD = 1;
             };
